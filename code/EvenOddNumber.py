b4dc78424138a4fa3859764e8fcef5e1b4e5a5c0
Ayush Kelkar <ayush94klkr@gmail.com>
2025-07-13 20:20:54 +0530

    Updated Notes.md and one Sample program for testing

---
+++ code/EvenOddNumber.py
@@ -0,0 +1,11 @@
+# To check if a number is even or not (Very Easy program, just to check git stuff)
+print("This program checks if the inputted number is even or odd")
+print(" To start, input the number:")
+num = int(input())
+if num == 0:
+    print("Number is Zero")
+else:
+    if num%2 == 0:
+        print("Number is Even")
+    else:
+        print("Number is Odd")
