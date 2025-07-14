dd389a53c13ba024e6804882123f16308d26c860
Ayush Kelkar <ayush94klkr@gmail.com>
2025-07-14 16:55:50 +0530

    Testing GitWrapper.exe v1.0

--- code/EvenOddNumber.py
+++ code/EvenOddNumber.py
@@ -12,4 +12,5 @@ else:
 # 13 July 2025 22:31 Testing for git using GitWrapper v0.5
 # 14 July 2025 15:40 Testing for git using GitWrapper v0.9.1
 # 14 July 2025 16:00 Testing for git using GitWrapper v0.9.2
-# 14 July 2025 16:35 Final Test for git using GitWrapper CLI v1.0
+# 14 July 2025 16:35 Testing for git using GitWrapper CLI v1.0
+# 14 July 2025 16:35 Final Test for git using GitWrapper exe (CLI Based) v1.0
